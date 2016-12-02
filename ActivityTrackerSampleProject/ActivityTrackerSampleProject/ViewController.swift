0b1e4fc9cb1c3b439b84c40976222a07a6b7d819
CarlNathan <c.n.udren@gmail.com>
2016-12-01 19:17:48 -0500

    Added button to demonstrate animation.

--- ActivityTrackerSampleProject/ActivityTrackerSampleProject/ViewController.swift
+++ ActivityTrackerSampleProject/ActivityTrackerSampleProject/ViewController.swift
@@ -13,11 +13,30 @@ class ViewController: UIViewController {
     let outerCicle = CircleSliderView()
     let middleCircle = CircleSliderView()
     let innerCircle = CircleSliderView()
-    
+    let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupCircles()
+        setupButton()
+    }
+    
+    func setupButton() {
+        button.backgroundColor = UIColor.white
+        button.setTitle("Animate", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.addTarget(self, action: #selector(buttonWasPressed), for: .touchUpInside)
+        button.layer.cornerRadius = 2
+        view.addSubview(button)
+    }
+    
+    func buttonWasPressed() {
+        outerCicle.applySettingsAndAnimate(afterDelay: 0.0, completion: {})
+        middleCircle.applySettingsAndAnimate(afterDelay: 0.5, completion: {})
+        innerCircle.applySettingsAndAnimate(afterDelay: 1.0, completion: {})
+    }
+    
+    func setupCircles() {
         //background color
         view.backgroundColor = UIColor.black
         
@@ -25,7 +44,7 @@ class ViewController: UIViewController {
         view.addSubview(outerCicle)
         view.addSubview(middleCircle)
         view.addSubview(innerCircle)
-
+        
         //setup cirles
         let thickness = 30
         let fraction1 = 0.9
@@ -42,7 +61,7 @@ class ViewController: UIViewController {
         outerCicle.configure(thickness: thickness, fraction: fraction1, color: color1, title: "Move", iconImage: rightImage)
         middleCircle.configure(thickness: thickness, fraction: fraction2, color: color2, title: "Exercise", iconImage: doubleRightImage)
         innerCircle.configure(thickness: thickness, fraction: fraction3, color: color3, title: "Stand", iconImage: upImage)
-        
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -70,6 +89,12 @@ class ViewController: UIViewController {
         let frame = outerCicle.frameThatFitsInside()
         middleCircle.frame = frame
         innerCircle.frame = middleCircle.frameThatFitsInside()
+        
+        
+        
+        //button
+        button.frame = CGRect(x: view.frame.width / 10, y: outerCicle.frame.maxY + 40, width: view.frame.width - view.frame.width / 5, height: 30)
+        
         
     }
     
